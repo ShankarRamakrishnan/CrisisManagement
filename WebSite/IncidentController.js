08cd1de6bbad834456a545fa95cded34e6e90d5d
git-shankarr.remya.gmail.com <git-shankarr.remya.gmail.com@chromium.org>
2015-06-29 21:43:45 +0530

    adding angular locale

--- WebSite/IncidentController.js
+++ WebSite/IncidentController.js
@@ -1,6 +1,10 @@
-﻿angular.module('CrisisManagment')
+﻿angular.module('CrisisManagement')
     .controller('IncidentCtrl', function($scope) {
-        $scope.loadIncident = function($scope) {
-            
-        }
-    });
+    $scope.loadIncident = function($scope) {
+
+    };
+
+    $scope.saveIncident = function ($scope, $http) {
+        console.log($scope.incident.name);
+    }
+});
